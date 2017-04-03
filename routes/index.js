0881cabe4b3dba39aa7d78004e6dd467016afe97
Baeyoonjeong <bae419@naver.com>
2017-04-04 00:18:40 +0900

    chap1

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  //res.render('test', { title: 'Express' });
+  res.render('Listing 01', { title: 'Express' });
+
+});
+
+module.exports = router;
